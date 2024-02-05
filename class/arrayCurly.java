ae69bdf34dfa80b1579fed6aafc9dbaba6c59b1b
Overkill005 <157211069+Overkill005@users.noreply.github.com>
2024-02-05 17:10:55 +0530

    18th drop

---
+++ class/arrayCurly.java
@@ -0,0 +1,7 @@
+public class arrayCurly {
+
+  public static void main(String[] args) {
+    int arr[] = new int[] { 10, 20, 30 };
+    System.out.println(arr[0]);
+  }
+}
---
+++ class/arrayInputSize.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class arrayInputSize {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter array length : ");
+    int x = sc.nextInt();
+    System.out.println("Enter " + x + " numbers : ");
+    int arr[] = new int[x];
+    for (int i = 0; i < x; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("Elements are : ");
+    for (int i = 0; i < x; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    sc.close();
+  }
+}
