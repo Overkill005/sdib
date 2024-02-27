0472c3a3b55c991651125e66ba7f43fb5e1007f9
Overkill005 <157211069+Overkill005@users.noreply.github.com>
2024-02-27 17:21:18 +0530

    32nd drop

---
+++ class/html/jquery/country_result.php
@@ -0,0 +1,16 @@
+<?php
+  $k = $_REQUEST['cu'];
+  if($k=="India"):
+?>
+
+<option>Odisha</option>
+<option>Gujarat</option>
+<?php endif;
+
+if($k== "Pakistan"):
+  ?>
+
+<option>Lahore</option>
+<option>Karachi</option>
+<?php endif;
+?>
---
+++ class/html/jquery/result.php
@@ -0,0 +1,4 @@
+<?php
+  $k = $_REQUEST['key'];
+  echo $k . " Rout";
+?>
